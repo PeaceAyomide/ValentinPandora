6f1ec3d8770206f315b68c710772fb9f2cf54d39
Peace Melodi <peacemelodi1@gmail.com>
2024-02-14 17:23:44 +0100

    make changes

--- src/valenvibrate/VVibrate.jsx
+++ src/valenvibrate/VVibrate.jsx
@@ -1,12 +1,18 @@
 import React, { useState, useRef }  from 'react'
 import './VVibrate.css'
 import vibrateSound from './vibrate.mp3'
+// pics
+import Skull from '../valenvibrate/skull.gif'
+import Love from '../valenvibrate/love.gif'
 
 const VVibrate = () => {
 
-  const [message, setMessage] = useState("Peace will you be my Valentine");
+  const [message, setMessage] = useState("Will you be my Valentine ?");
+  const [noMessage, setNoMessage] = useState(false)
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
   const audioRef = useRef(new Audio(vibrateSound));
+  const [showSkull, setShowSkull] = useState(false);
+const [showLove, setShowLove] = useState(false)
 
   const handleClickno = () => {
     // This for normal vibration not audio
@@ -19,23 +25,39 @@ const VVibrate = () => {
     audioRef.current.loop = true; // Set looping to true
     audioRef.current.play(); // Start playing the audio
     setIsAudioPlaying(true); // Update the state to indicate audio is playing
+    setMessage(false)
+    setNoMessage("⚠️VIRUS is now being Downloaded on your phone, Good Luck lol")
+    setShowSkull(true);
+    setShowLove(false);
   }
 
   const handleClickyes = () => { 
-    setMessage("Thank you for accepting me as your valentine");
+    setMessage("Thank you for accepting me as your Val");
     
     if (isAudioPlaying && audioRef.current) {
       audioRef.current.pause(); // Pause the audio
       setIsAudioPlaying(false);
     }
+    setShowSkull(false);
+    setShowLove(true);
+    setNoMessage(false);
   }
   
   return (
       <div>
           <div className="contentval">
         <p>{message}</p>
-              <div className="btn">
-              <button className='yesbtn'  onClick={handleClickyes}>YES ❤️</button>
+        <div className="nomessback">
+        <p className='nomess'>{noMessage}</p>
+        </div>
+         <div className="btn">
+          {showLove && (<div className="love"> <img src={Love} alt="" /></div>)}
+          <button className='yesbtn' onClick={handleClickyes}>YES ❤️</button>
+          {showSkull && (
+            <div className="skull">
+              <img src={Skull} alt="Skull" />
+            </div>
+          )}
           <button className='nobtn' onClick={handleClickno}>NO 💀</button>    
               </div>
               </div>     
